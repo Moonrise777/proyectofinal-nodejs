8ba190b43a22c548ff0b496988498d267dd1a8c4
Arely <onixuniverse@gmail.com>
2024-06-26 00:27:56 -0600

    cambios en index.js envio de nodemailer

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Configuración de Nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'codexno1company@gmail.com',
+        pass: 'ecdvttmlrrhnzcwf'
+    }
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message, options } = req.body;
+
+    const mailOptions = {
+        from: 'codexno1company@gmail.com',
+        to: email,
+        subject: 'Nuevo mensaje de contacto',
+        text: `Nombre: ${name}\nEmail: ${email}\nMensaje: ${message}\nOpciones: ${options}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.send('Correo enviado: ' + info.response);
+    });
+});
+
+app.listen(3001, () => {
+    console.log('Servidor corriendo en el puerto 3001');
+});
